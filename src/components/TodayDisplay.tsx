bec98acc390de2ca8ff31fb7081afb82c9ed8723
satheeshpolu <satheeshpoluab@gmail.com>
2025-07-20 13:45:18 +0200

    Added date and time widgets

---
+++ src/components/TodayDisplay.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TodayDisplay: React.FC = () => {
+  const today = new Date();
+
+  const day = today.getDate();
+  const month = today.toLocaleString("default", { month: "long" }); // "July"
+  const year = today.getFullYear();
+
+  return (
+    <div style={{ textAlign: "center", fontFamily: "monospace", color: "#rgb(4 89 81)" }}>
+      <div style={{ fontSize: "4rem", fontWeight: "bold" }}>{day} {today.toString().split(' ')[0]}</div> 
+      <div style={{ fontSize: "2rem" }}>
+        {month} <span style={{ margin: "0 0.25rem" }}>|</span> {year}
+      </div>
+    </div>
+  );
+};
+
+export default TodayDisplay;
